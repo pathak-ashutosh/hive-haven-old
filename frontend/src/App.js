3a1d1f8fad40535b042087fe6a9b610412e9dda0
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2024-02-19 13:38:02 -0500

    Added Properties back

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar.js';
 import Footer from './components/Footer/Footer.js';
-import Register from './pages/Register/index.js';
-import Login from './pages/Login/index.js';
-import Main from './pages/Main/index.js';
-import Properties from './pages/Properties/index.js';
-import Property from './pages/Property/index.js';
+import Register from './Pages/Register/index.js';
+import Login from './Pages/Login/index.js';
+import Main from './Pages/Main/index.js';
+import Properties from './Pages/Properties/index.js';
+import Property from './Pages/Property/index.js';
 
 function App() {
   return (
