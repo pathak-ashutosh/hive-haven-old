5d55ee26e748575d86ad28796c49d1b9d4490d91
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2024-02-22 14:28:47 -0500

    Add environment variable

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {
     'Authorization': 'Bearer ' + localStorage.getItem('token'),
     'Content-Type': 'application/json'
