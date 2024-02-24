462dd0793d92509966b1ea2b24727461c0e75a96
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2024-02-24 12:46:12 -0500

    Installed Material UI

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
     <App />
-  </React.StrictMode>
+  </ThemeProvider>
 );
---
+++ frontend/src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2a0c4e',
+    },
+    secondary: {
+      main: '#f5f8de',
+    },
+    error: {
+      main: '#9e2b25',
+    },
+  },
+});
+
+export default theme;
