c5a48771d3b804171f9e37c58b3ca2748b92ec09
Ashutosh Pathak <iamashutoshpathak@gmail.com>
2024-05-20 14:40:54 -0400

    Fixed unresponsive mobile nav menu

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -42,7 +42,6 @@ const Nav = () => {
     };
   }, []);
 
-
   return (
     <header
       className={`px-12 max-md:px-8 py-4 fixed z-20 w-full transition-shadow duration-300 bg-white ${
@@ -52,7 +51,10 @@ const Nav = () => {
       <nav className="flex justify-between items-center">
         <div className="flex flex-col justify-between md:flex-row w-full">
           {/* Logo */}
-          <Link to="/" className="flex flex-col md:flex-row gap-4 max-md:pl-6 items-center">
+          <Link
+            to="/"
+            className="flex flex-col md:flex-row gap-4 max-md:pl-8 items-center"
+          >
             {/* <img src={headerLogo} alt="Hive Haven Logo" width={30} height={30} /> */}
             <p className="block text-xl font-bold hover:drop-shadow-sm">
               <span className="text-yellow-500">hive</span>Haven
@@ -62,23 +64,27 @@ const Nav = () => {
           {/* Navigation Links */}
           <ul
             ref={menuRef}
-            className={`flex flex-col sm:flex-row sm:gap-8 gap-4 max-md:hidden items-center text-md ${
+            className={`flex flex-row gap-8 max-md:hidden items-center text-md ${
               isMenuOpen ? "block" : "hidden"
             } md:flex`}
           >
             <li>
-              <Link to="/about" className="px-4" onClick={() => setIsMenuOpen(false)}>About</Link>
+              <Link to="/about" className="px-4">
+                About
+              </Link>
             </li>
             <li>
-              <Link to="/contact" className="px-4" onClick={() => setIsMenuOpen(false)}>Contact</Link>
+              <Link to="/contact" className="px-4">
+                Contact
+              </Link>
             </li>
             <li>
-              <Link to="/signup" onClick={() => setIsMenuOpen(false)}>
+              <Link to="/signup">
                 <PrimaryButton label="Sign Up" Icon={null} />
               </Link>
             </li>
             <li>
-              <Link to="/login" onClick={() => setIsMenuOpen(false)}>
+              <Link to="/login">
                 <SecondaryButton label="Log In" Icon={null} />
               </Link>
             </li>
@@ -96,21 +102,44 @@ const Nav = () => {
 
       {/* Mobile Menu */}
       {isMenuOpen && (
-        <ul className="pt-5 flex flex-col gap-4 items-center text-md md:hidden">
-          <li>
-            <Link to="/about" className="p-4" onClick={() => setIsMenuOpen(false)}>About</Link>
+        <ul
+          ref={menuRef}
+          className="pt-5 flex flex-col gap-4 items-stretch text-md md:hidden w-full"
+        >
+          <li className="border py-3 rounded-3xl text-center">
+            <Link
+              to="/about"
+              className="p-4"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              About
+            </Link>
           </li>
-          <li>
-            <Link to="/contact" className="p-4" onClick={() => setIsMenuOpen(false)}>Contact</Link>
+          <li className="border py-3 rounded-3xl text-center">
+            <Link
+              to="/contact"
+              className="p-4"
+              onClick={() => setIsMenuOpen(false)}
+            >
+              Contact
+            </Link>
           </li>
-          <div className="flex gap-3">
+          <div className="flex flex-col gap-3 items-stretch">
             <li>
-              <Link to="/signup" onClick={() => setIsMenuOpen(false)}>
+              <Link
+                to="/signup"
+                onClick={() => setIsMenuOpen(false)}
+                className="flex flex-col items-stretch"
+              >
                 <PrimaryButton label="Sign Up" Icon={null} />
               </Link>
             </li>
             <li>
-              <Link to="/login" onClick={() => setIsMenuOpen(false)}>
+              <Link
+                to="/login"
+                onClick={() => setIsMenuOpen(false)}
+                className="flex flex-col items-stretch"
+              >
                 <SecondaryButton label="Log In" Icon={null} />
               </Link>
             </li>
